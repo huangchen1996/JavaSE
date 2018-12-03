769593482d39c405eea0631a8afbcad6e9607740
huangchen1996 <huangchen19960227@163.com>
2018-12-03 20:40:34 +0800

    关键字的概述和使用

---
+++ src/Chapter2_BasicGramma/KeyWord_01/KeyWordDemo_01.java
@@ -0,0 +1,13 @@
+/**
+ * 关键字:被java语言赋予特定含义的单词
+ *
+ * 特点:组成关键字单词的字母全部小写
+ *
+ * 注意:
+ *     关键字有特殊的颜色标记
+ */
+Public class KeyWordDemo_01{
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
